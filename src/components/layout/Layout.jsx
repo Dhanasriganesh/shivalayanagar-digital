f5d0380d9343dc27e6a61fe98afc6fea9afe4de9
dhanasriganesh <dhanasriganesh9@gmail.com>
2026-07-24 22:20:38 +0530

    scroll

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,23 @@
-import { BrowserRouter as Router } from 'react-router-dom'
+import { useEffect } from 'react'
+import { BrowserRouter as Router, useLocation } from 'react-router-dom'
 import Header from '../header/Header'
 import Footer from '../footer/Footer'
 import Routers from '../routers/Routers'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' })
+  }, [pathname])
+
+  return null
+}
+
 function Layout() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="flex min-h-dvh flex-col">
         <Header />
         <main className="mx-auto w-full max-w-3xl flex-1 px-4 py-5 sm:px-5 sm:py-7">
